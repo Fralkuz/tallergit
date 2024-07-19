b8e104123ccc4990c5537da3fe33f1621db368e1
fralkuz <franjauz@gmail.com>
2024-07-19 14:32:56 -0500

    cambios efectuados

--- prueba.py
+++ prueba.py
@@ -1,3 +1,5 @@
 # programa debe contener 3 funciones
 
 # debemos crear un formulario para la captura de datos
+
+#listo la informacion va a ser tenida en cuenta voy a crear un archivo para el formulario
