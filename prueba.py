7ba855634d89dc3ecbf15c7a146de5ab8c243875
fralkuz <franjauz@gmail.com>
2024-07-19 12:01:33 -0500

    empezando comunicacion

--- prueba.py
+++ prueba.py
@@ -1,15 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-echo "solo esta este escrito"
-
-
-function (){
-    return ("otro intento")
-}
-=======
-#he borrado todo
->>>>>>> e71629ee01afaadf8353a38b11b044b473083bbf
-#vamos a hacer otra prueba de comunicacion
-=======
-#hemos empezado a realizar otras pruebas para verificar las transacciones
->>>>>>> 4ced073bad3561831bffc602892b2ad5742b982a
+#vamos a empezar
